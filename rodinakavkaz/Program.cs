bbb13dc2bd6878f7a3f7b44f4b772eb32840561d
Dauren <d.k.nurgaziev@gmail.com>
2024-09-24 11:07:22 +0500

    1 exercise

---
+++ rodinakavkaz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace rodinakavkaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());    
+
+            if(Math.Pow(a,2) == b)
+            {
+                Console.WriteLine("рабетаем дура");
+            }
+            else
+            {
+                Console.WriteLine("ахуеть");
+            }
+        }
+    }
+}
