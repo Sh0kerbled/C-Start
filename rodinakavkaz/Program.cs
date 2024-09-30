77e9d408aae2cf70636389b05b4e844f5ff682ba
Dauren <d.k.nurgaziev@gmail.com>
2024-09-30 23:02:21 +0500

    lesson 3

--- rodinakavkaz/Program.cs
+++ rodinakavkaz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace rodinakavkaz
 {
@@ -87,12 +88,99 @@ namespace rodinakavkaz
                 Console.WriteLine($"{second} больше чем {first}");
             }*/
 
-            string n = Console.ReadLine();
+            /*string n = Console.ReadLine();
 
             for (int i = 0; i < n.Length; i++)
             {
                 //неа не вывез
+            }*/
+
+
+
+            /*int[] array = { 1, -5, 6, -7, 3, 6 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool isFound = false;
+
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == num)
+                {
+                    isFound = true;
+                    break;
+                }
+                
             }
+
+            string result = isFound == true ? "работаем дура" : "пошгелн аъуй";
+            Console.WriteLine(result);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool isFound = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                {
+                    isFound = true;
+                }
+            }
+            string result = isFound == true ? "да" : "нет";
+            Console.WriteLine($"r=esult is {result}");*/
+
+            /*int[] array = { 1, -5, 6, -7, 3 };
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = -array[i];
+                Console.Write(array[i]+ ", ");
+            }*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = array[i] + array[i - i];
+                Console.Write(array[i] + ", ");
+            }*/
+
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i]++;
+                Console.Write(array[i] + ", ");
+            }
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in array)
+            {
+                if(num == number)
+                {
+                    Console.WriteLine("работаем дура");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("не работаем дура");
+                }
+            }
+
+            for(int k = 0; k < array.Length; k++)
+            {
+                array[k] = -array[k];
+                Console.Write(array[k] + ", ");
+            }
+
+            int[] array1 = { 5, -3, 9, -1, 2, -7, 0 };
+            for(int g = 0; g < array1.Length; g++)
+            {
+                if(array1[g] >= 0)
+                {
+                    Console.Write(array1[g] + " ");
+                }
+            }
+            
         }
     }
 }
